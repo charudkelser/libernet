9a1a4415bc9d12c045fd6a8edf930b3f2ecd5808
charudkelser <charudkelser@gmail.com>
2026-07-30 09:42:42 +0700

    Refactor install.sh for OpenWrt and PHP 8 compatibility
    Updated the installer script to improve compatibility with OpenWrt and PHP 8. Removed unnecessary functions and added new installation steps for Libernet.

--- install.sh
+++ install.sh
@@ -1,206 +1,88 @@
-#!/bin/bash
+#!/bin/sh
 
-# Libernet Installer
-# by Lutfa Ilham
-# v1.0.0
+echo "=========================================="
+echo "   Installing Libernet Modern (PHP 8)     "
+echo "=========================================="
 
-if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root" 1>&2
-  exit 1
+# 1. Update repo & install dependensi PHP 8 resmi OpenWrt
+opkg update && opkg install php8 php8-cgi php8-mod-session php8-mod-curl php8-mod-mbstring php8-mod-fileinfo php8-mod-zip httping resolveip libxml2
+
+# 2. Download Libernet dari Repo Kamu & Atur Folder
+rm -rf /root/libernet /www/libernet /tmp/libernet.zip /tmp/libernet-main
+curl -sL https://github.com/charudkelser/libernet/archive/refs/heads/main.zip -o /tmp/libernet.zip
+unzip -q /tmp/libernet.zip -d /tmp/
+mv /tmp/libernet-main /root/libernet
+
+cp -rf /root/libernet/system/libernet/* /root/libernet/ 2>/dev/null
+[ -d /root/libernet/system/bin ] && cp -rf /root/libernet/system/bin/* /usr/bin/
+[ -d /root/libernet/system/init.d ] && cp -rf /root/libernet/system/init.d/* /etc/init.d/
+
+ln -sf /root/libernet /www/libernet
+chmod -R 755 /root/libernet
+[ -f /usr/bin/libernet ] && chmod +x /usr/bin/libernet
+[ -f /etc/init.d/libernet ] && chmod +x /etc/init.d/libernet
+rm -rf /tmp/libernet.zip /tmp/libernet-main
+
+# 3. Setup Interface & Firewall Libernet (Fungsi Asli Dev)
+if ! uci get network.libernet > /dev/null 2>&1; then
+  echo "Mengonfigurasi firewall Libernet..."
+  uci set network.libernet=interface
+  uci set network.libernet.proto='none'
+  uci set network.libernet.ifname='tun1'
+  uci commit
+  uci add firewall zone
+  uci set firewall.@zone[-1].network='libernet'
+  uci set firewall.@zone[-1].name='libernet'
+  uci set firewall.@zone[-1].masq='1'
+  uci set firewall.@zone[-1].mtu_fix='1'
+  uci set firewall.@zone[-1].input='REJECT'
+  uci set firewall.@zone[-1].forward='REJECT'
+  uci set firewall.@zone[-1].output='ACCEPT'
+  uci commit
+  uci add firewall forwarding
+  uci set firewall.@forwarding[-1].src='lan'
+  uci set firewall.@forwarding[-1].dest='libernet'
+  uci commit
+  /etc/init.d/network restart
 fi
 
-HOME="/root"
-ARCH="$(grep 'DISTRIB_ARCH' /etc/openwrt_release | awk -F '=' '{print $2}' | sed "s/'//g")"
-LIBERNET_DIR="${HOME}/libernet"
-LIBERNET_WWW="/www/libernet"
-STATUS_LOG="${LIBERNET_DIR}/log/status.log"
-DOWNLOADS_DIR="${HOME}/Downloads"
-LIBERNET_TMP="${DOWNLOADS_DIR}/libernet"
-REPOSITORY_URL="https://github.com/lutfailham96/libernet"
+# 4. Setting Web Server uHTTPd & PHP 8
+ln -sf /usr/bin/php8-cgi /usr/bin/php-cgi
+uci set uhttpd.main.interpreter='.php=/usr/bin/php-cgi'
+uci commit uhttpd
+sed -i 's/disable_functions =.*/disable_functions =/g' /etc/php.ini
 
-function install_packages() {
-  while IFS= read -r line; do
-    # install package if not installed yet
-    if [[ $(opkg list-installed "${line}" | grep -c "${line}") != "1" ]]; then
-      opkg install "${line}"
-    fi
-  done < requirements.txt
-}
+# 5. Pasang Menu LuCI Modern (JS View)
+mkdir -p /usr/lib/lua/luci/controller /www/luci-static/resources/view
+cat <<'EOF' >/usr/lib/lua/luci/controller/libernet.lua
+module("luci.controller.libernet", package.seeall)
+function index()
+    entry({"admin","services","libernet"}, view("libernet"), _("Libernet"), 55).leaf = true
+end
+EOF
 
-function install_proprietary_binaries() {
-  echo -e "Installing proprietary binaries"
-  while IFS= read -r line; do
-    if ! which ${line} > /dev/null 2>&1; then
-      bin="/usr/bin/${line}"
-      echo "Installing ${line} ..."
-      curl -sLko "${bin}" "https://github.com/lutfailham96/libernet-proprietary/raw/main/${ARCH}/binaries/${line}"
-      chmod +x "${bin}"
-    fi
-  done < binaries.txt
-}
+cat <<'EOF' >/www/luci-static/resources/view/libernet.js
+'use strict';
+'require view';
+return view.extend({
+  render: function() {
+    return E('div', { 'class': 'cbi-section' }, [
+      E('h2', {}, 'Libernet'),
+      E('iframe', {
+        id: 'libernet',
+        src: 'http://' + window.location.hostname + '/libernet',
+        style: 'width:100%;min-height:650px;border:none;border-radius:2px;'
+      })
+    ]);
+  }
+});
+EOF
 
-function install_proprietary_packages() {
-  echo -e "Installing proprietary packages"
-  while IFS= read -r line; do
-    if ! which ${line} > /dev/null 2>&1; then
-      pkg="/tmp/${line}.ipk"
-      echo "Installing ${line} ..."
-      curl -sLko "${pkg}" "https://github.com/lutfailham96/libernet-proprietary/raw/main/${ARCH}/packages/${line}.ipk"
-      opkg install "${pkg}"
-      rm -rf "${pkg}"
-    fi
-  done < packages.txt
-}
+# 6. Restart Service & Clean Cache
+rm -f /tmp/luci-indexcache*; rm -rf /tmp/luci-modulecache
+service uhttpd restart
+[ -f /etc/init.d/libernet ] && /etc/init.d/libernet restart
 
-function install_proprietary() {
-  install_proprietary_binaries
-  install_proprietary_packages
-}
-
-function install_prerequisites() {
-  # update packages index
-  opkg update
-}
-
-function install_requirements() {
-  echo -e "Installing packages" \
-    && install_prerequisites \
-    && install_packages \
-    && install_proprietary
-}
-
-function enable_uhttp_php() {
-  if ! grep -q ".php=/usr/bin/php-cgi" /etc/config/uhttpd; then
-    echo -e "Enabling uhttp php execution" \
-      && uci set uhttpd.main.interpreter='.php=/usr/bin/php-cgi' \
-      && uci add_list uhttpd.main.index_page='index.php' \
-      && uci commit uhttpd \
-      && echo -e "Restarting uhttp service" \
-      && /etc/init.d/uhttpd restart
-  else
-    echo -e "uhttp php already enabled, skipping ..."
-  fi
-}
-
-function add_libernet_environment() {
-  if ! grep -q LIBERNET_DIR /etc/profile; then
-    echo -e "Adding Libernet environment" \
-      && echo -e "\n# Libernet\nexport LIBERNET_DIR=${LIBERNET_DIR}" | tee -a '/etc/profile'
-  fi
-}
-
-function install_libernet() {
-  # stop Libernet before install
-  if [[ -f "${LIBERNET_DIR}/bin/service.sh" && $(cat "${STATUS_LOG}") != "0" ]]; then
-    echo -e "Stopping Libernet"
-    "${LIBERNET_DIR}/bin/service.sh" -ds > /dev/null 2>&1
-  fi
-  # removing directories that might contains garbage
-  rm -rf "${LIBERNET_WWW}"
-  # install Libernet
-  echo -e "Installing Libernet" \
-    && mkdir -p "${LIBERNET_DIR}" \
-    && echo -e "Copying updater script" \
-    && cp -avf update.sh "${LIBERNET_DIR}/" \
-    && echo -e "Copying binary" \
-    && cp -arvf bin "${LIBERNET_DIR}/" \
-    && echo -e "Copying system" \
-    && cp -arvf system "${LIBERNET_DIR}/" \
-    && echo -e "Copying log" \
-    && cp -arvf log "${LIBERNET_DIR}/" \
-    && echo -e "Copying web files" \
-    && mkdir -p "${LIBERNET_WWW}" \
-    && cp -arvf web/* "${LIBERNET_WWW}/" \
-    && echo -e "Configuring Libernet" \
-    && sed -i "s/LIBERNET_DIR/$(echo ${LIBERNET_DIR} | sed 's/\//\\\//g')/g" "${LIBERNET_WWW}/config.inc.php"
-}
-
-function configure_libernet_firewall() {
-  if ! uci get network.libernet > /dev/null 2>&1; then
-    echo "Configuring Libernet firewall" \
-      && uci set network.libernet=interface \
-      && uci set network.libernet.proto='none' \
-      && uci set network.libernet.ifname='tun1' \
-      && uci commit \
-      && uci add firewall zone \
-      && uci set firewall.@zone[-1].network='libernet' \
-      && uci set firewall.@zone[-1].name='libernet' \
-      && uci set firewall.@zone[-1].masq='1' \
-      && uci set firewall.@zone[-1].mtu_fix='1' \
-      && uci set firewall.@zone[-1].input='REJECT' \
-      && uci set firewall.@zone[-1].forward='REJECT' \
-      && uci set firewall.@zone[-1].output='ACCEPT' \
-      && uci commit \
-      && uci add firewall forwarding \
-      && uci set firewall.@forwarding[-1].src='lan' \
-      && uci set firewall.@forwarding[-1].dest='libernet' \
-      && uci commit \
-      && /etc/init.d/network restart
-  fi
-}
-
-function configure_libernet_service() {
-  echo -e "Configuring Libernet service"
-  # disable services startup
-  # DoT
-  /etc/init.d/stubby disable
-  # shadowsocks
-  /etc/init.d/shadowsocks-libev disable
-  # openvpn
-  /etc/init.d/openvpn disable
-  # stunnel
-  /etc/init.d/stunnel disable
-}
-
-function setup_system_logs() {
-  echo -e "Setup system logs"
-  logs=("status.log" "service.log" "connected.log")
-  for log in "${logs[@]}"; do
-    if [[ ! -f "${LIBERNET_DIR}/log/${log}" ]]; then
-      touch "${LIBERNET_DIR}/log/${log}"
-    fi
-  done
-}
-
-function finish_install() {
-  router_ip="$(ifconfig br-lan | grep 'inet addr:' | awk '{print $2}' | awk -F ':' '{print $2}')"
-  echo -e "Libernet successfully installed!\nLibernet URL: http://${router_ip}/libernet"
-}
-
-function main_installer() {
-  install_requirements \
-    && install_libernet \
-    && add_libernet_environment \
-    && enable_uhttp_php \
-    && configure_libernet_firewall \
-    && configure_libernet_service \
-    && setup_system_logs \
-    && finish_install
-}
-
-function main() {
-  # install git if it's unavailable
-  if [[ $(opkg list-installed git | grep -c git) != "1" ]]; then
-    opkg update \
-      && opkg install git
-  fi
-  if [[ $(opkg list-installed git-http | grep -c git-http) != "1" ]]; then
-    opkg update \
-      && opkg install git-http
-  fi
-  # create ~/Downloads directory if not exist
-  if [[ ! -d "${DOWNLOADS_DIR}" ]]; then
-    mkdir -p "${DOWNLOADS_DIR}"
-  fi
-  # install Libernet
-  if [[ ! -d "${LIBERNET_TMP}" ]]; then
-    git clone --depth 1 "${REPOSITORY_URL}" "${LIBERNET_TMP}" \
-      && cd "${LIBERNET_TMP}" \
-      && bash install.sh
-  else
-    cd "${LIBERNET_TMP}" \
-      && main_installer
-  fi
-}
-
-main
+echo "=========================================="
+echo "   Libernet Success Installed! Enjoy!     "
+echo "=========================================="
